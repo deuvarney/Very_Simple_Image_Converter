663fe6f18b2c62e1bf5cd7121a7e0cc59d4e6687
deuvarney <deuvarney@yahoo.com>
2024-12-12 23:29:44 -0500

    Update config to build project with Github Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+nextConfig.assetPrefix = assetPrefix
+nextConfig.basePath = basePath
+nextConfig.output = 'export'
+
 export default nextConfig;
